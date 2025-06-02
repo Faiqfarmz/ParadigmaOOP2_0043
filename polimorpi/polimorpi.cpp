8457f546790ee85d157ac67fc15b0c4feab1afdc
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-02 11:09:03 +0700

    class seseorang polimorpi

---
+++ polimorpi/polimorpi.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+class seseorang {
+    public:
+        virtual void pesan() = 0;
+        // virtual void persan() {
+        //  cout << "pesan dari seseorang" << endl;
+        //}
+};
