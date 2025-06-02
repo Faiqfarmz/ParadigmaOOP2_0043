19260bd2d89186701ae02844de2688f6d41658b3
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-02 09:55:25 +0700

    class AbstraksiKlas

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,12 +5,3 @@ using namespace std;
 class AbstraksiKlas {
 private: string x, y;
 
-public:
-
-    // method untuk mengisi nilai
-    // privat member
-    void setXY(string a, string b) {
-        x = a;
-        y = b;
-    }
-}
