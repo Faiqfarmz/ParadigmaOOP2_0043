750b9e4786923b864891b7f5a96dcefaff462df0
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-02 10:50:46 +0700

    membuat baseClass noPewarisan

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+using namespace std;
+
+class baseClass {
+    public:
+        virtual void perkenalan() {
+            cout << "hallo saya Function dari base class";
+        }
+};
